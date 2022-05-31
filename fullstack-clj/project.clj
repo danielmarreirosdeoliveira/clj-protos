24878705d2062d3d28a48224953fe24d1bcd632e
Dan <dan@eighttrigrams.net>
2022-05-31 20:12:06 +0100

    Update dependencies

--- fullstack-clj/project.clj
+++ fullstack-clj/project.clj
@@ -7,7 +7,7 @@
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/clojurescript "1.10.773"]
+                 [org.clojure/clojurescript "1.11.54"]
                  [compojure/compojure "1.6.3"]
                  [ring/ring-json "0.5.1"]
                  [reagent "1.1.1" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
@@ -15,9 +15,10 @@
                  [cljs-ajax "0.8.4"]
                  [clj-http "3.12.3"]
                  [secretary "1.2.3"]
+                 [com.fasterxml.jackson.core/jackson-core "2.13.3"]
                  [venantius/accountant "0.2.5"]
                  [buddy/buddy-auth "3.0.323"]
-                 [ring/ring-jetty-adapter "1.8.2"]
+                 [ring/ring-jetty-adapter "1.9.5"]
                  [derekchiang/ring-proxy "1.0.1"]]
 
   :plugins [[lein-ring "0.12.5"]]
@@ -31,7 +32,7 @@
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "fullstack.test-runner"]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.12"]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.18"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
                    :resource-paths ["target"]
                    :clean-targets ^{:protect false} ["target" "resources/public/cljs-out"]}})
